18729b6d68be6399e55cfc77832d4c5b7c0932a3
Valdemar Ceccon <valdemar.ceccon@gmail.com>
2021-05-17 22:54:27 -0300

    wip game state

--- src/game.rs
+++ src/game.rs
@@ -3,28 +3,36 @@ use super::board;
 pub struct Game {
     board: board::Board,
     current_player: board::Player,
-    winner: Option<board::Player>,
 }
 
 const ROWS: usize = 6;
 const COLS: usize = 7;
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub enum GameState {
+    PLAYING,
+    TIE,
+    WINNER,
+}
 
 impl Game {
     pub fn new(first_player: board::Player) -> Self {
         Game {
             board: board::Board::new(ROWS, COLS),
             current_player: first_player,
-            winner: None,
         }
     }
 
-    pub fn play(&mut self, col: usize) -> Result<(), board::PlayErr> {
+    pub fn play(&mut self, col: usize) -> Result<GameState, board::PlayErr> {
         self.board.play(col, self.current_player)?;
         self.current_player = match self.current_player {
             board::Player::YELLOW => board::Player::RED,
             board::Player::RED => board::Player::YELLOW,
         };
-        Ok(())
+        if self.board.is_board_full() {
+            return Ok(GameState::TIE);
+        }
+        Ok(GameState::PLAYING)
     }
 }
 
@@ -47,6 +55,22 @@ mod test {
 
         game.play(col)?;
         assert_eq!(game.current_player, board::Player::YELLOW);
+
+        Ok(())
+    }
+
+    #[test]
+    fn game_finish_in_tie() -> Result<(), board::PlayErr> {
+        let mut game = Game::new(board::Player::YELLOW);
+        let mut game_state = GameState::PLAYING;
+        for c in 0..COLS {
+            for _ in 0..ROWS {
+                assert_eq!(game_state, GameState::PLAYING);
+                game_state = game.play(c)?;
+            }
+        }
+
+        assert_eq!(game_state, GameState::TIE);
 
         Ok(())
     }
